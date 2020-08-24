d3a667cef1e23c88de68b953657ea0192d318904
lilianyangc <mlilianyang@gmail.com>
2020-08-23 22:22:19 -0400

    ItemCards added

--- src/components/ItemList/ItemCards/ItemCards.js
+++ src/components/ItemList/ItemCards/ItemCards.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import ItemCard from '../ItemCard/ItemCard'
 import classes from './ItemCards.module.css';
+import Salad from '../../../assets/images/salad.png'
+import Fries from '../../../assets/images/fries.jpg'
+import Soda from '../../../assets/images/soda.png'
+
+
 
 export default function ItemCards() {
     return (
-        <ul className={classes.ItemCards}>
-            <p>Item Cards</p>
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-        </ul>
+        <diV className={classes.ItemCards}>
+            <ul>
+                {/* <p>Item Cards</p> */}
+                <ItemCard itemName="salad" url ={Salad}/>
+                <ItemCard itemName="fries" url ={Fries}/>
+                <ItemCard itemName="soda" url ={Soda}/>
+            </ul>
+        </diV>
+        
     )
 }
